92453ef0040d0e427c9e9772099b12f5e6d36a71
Jerome Fosse <jerome@fosse.nom.fr>
2024-05-18 02:17:30 +0200

    doc + cmp configuration

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -73,11 +73,8 @@ return require('packer').startup(function(use)
 
   -- completion plugins
   use "hrsh7th/nvim-cmp"                          -- The completion plugin
-  use "hrsh7th/cmp-buffer"                        -- buffer completions
   use "hrsh7th/cmp-path"                          -- path completions
-  use "hrsh7th/cmp-cmdline"                       -- cmdline completions
   use "hrsh7th/cmp-nvim-lua"                      -- NeoVim completions
-  use "saadparwaiz1/cmp_luasnip"                  -- snippet completions
   use "hrsh7th/cmp-nvim-lsp"
 
   -- snippets
--- lua/plugins/cmp/init.lua
+++ lua/plugins/cmp/init.lua
@@ -15,11 +15,6 @@ end
 
 require("luasnip/loaders/from_vscode").lazy_load()
 
-local check_backspace = function()
-  local col = vim.fn.col "." - 1
-  return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
-end
-
 cmp.setup({
   snippet = {
     -- REQUIRED - you must specify a snippet engine
@@ -32,31 +27,14 @@ cmp.setup({
     documentation = cmp.config.window.bordered(),
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-k>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-j>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping(
-      function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        elseif luasnip.expandable() then
-          luasnip.expand()
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        elseif check_backspace() then
-          fallback()
-        else
-          fallback()
-        end
-      end, { "i", "s" }
-    )
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'luasnip' }, -- For luasnip users.
-    { name = 'buffer' },
     { name = 'nvim_lua' },
     { name = 'path' },
   }),
@@ -66,43 +44,16 @@ cmp.setup({
       maxwidth = 50,
       ellipsis_char = "...",
       before = function (entry, vim_item)
-        vim_item.menu = ({
-          nvim_lsp = "[LSP]",
-          nvim_lua = "[Nvim]",
-          luasnip = "[Snippet]",
-          buffer = "[Buffer]",
-          path = "[Path]",
-        })[entry.source.name]
+--        vim_item.menu = ({
+--          nvim_lsp = "[LSP]",
+--          nvim_lua = "[Nvim]",
+--          luasnip = "[Snippet]",
+--          buffer = "[Buffer]",
+--          path = "[Path]",
+--        })[entry.source.name]
         return vim_item
       end,
     }),
   },
 })
-
--- Set configuration for specific filetype.
---cmp.setup.filetype('gitcommit', {
---    sources = cmp.config.sources({
---        { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
---    }, {
---        { name = 'buffer' },
---    })
---})
-
--- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
---cmp.setup.cmdline({ '/', '?' }, {
---    mapping = cmp.mapping.preset.cmdline(),
---    sources = {
---        { name = 'buffer' }
---    }
---})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
---cmp.setup.cmdline(':', {
---    mapping = cmp.mapping.preset.cmdline(),
---    sources = cmp.config.sources({
---        { name = 'path' }
---    }, {
---        { name = 'cmdline' }
---    })
---})
 
